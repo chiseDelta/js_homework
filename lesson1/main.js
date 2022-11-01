25987eefa4b0928508ff228c2c90e70274370df4
chiseDelta <ghostymyname@gmail.com>
2022-11-01 14:51:34 +0200

    створив змінні і присвоїв їм значення. + вивів в консоль (console.log())

---
+++ lesson1/main.js
@@ -0,0 +1,43 @@
+/* Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16,
+ true, false. Вивести кожну змінну за допомогою: console.log */
+
+
+let word1 = 'hello'; //string - '' "" `` (напис на заборі)
+console.log(word1); // логування (щоб перевизначити змінну - word1 = 'bye';)
+
+let word2 = 'owu';
+console.log(word2);
+
+let word3 = 'com';
+console.log(word3);
+
+let word4 = 'ua';
+console.log(word4);
+
+let num1 = '1';
+console.log(num1);
+
+let num2 = '10';
+console.log(num2);
+
+let num3 = '-999';
+console.log(num3);
+
+let num4 = '123';
+console.log(num4);
+
+let PI = '3.14';
+console.log(PI);
+
+let num6 = '2.7';
+console.log(num6);
+
+let num7 = '16';
+console.log(num7);
+
+let t = 'true';
+console.log(t);
+
+let f = 'false';
+console.log(f);
+
