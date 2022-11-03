1381f7825df359fbe1587c4e1ac380cbdd9f3fa9
chiseDelta <ghostymyname@gmail.com>
2022-11-03 16:09:55 +0200

    забув про коміти и зробив одразу 2 завдання. #QUOKKATOP зробив масив і об'єкти

---
+++ lesson2/index.js
@@ -0,0 +1,25 @@
+// Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let randomElements = [7, true, false, 'coffee', 'rickroll', '3.14', 314, false, [0, 1, 2, 3], 'rE']; // [] - масив
+console.log(randomElements);
+
+// Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+let book1 = { // {} - об'єкт
+    title: 'Harry Potter and the Order of the Phoenix',
+    pageCount: 766,
+    genre: 'Fantasy'
+}
+
+let book2 = {
+    title: 'A History of the Modern World (9th Edition)',
+    pageCount: 1248,
+    genre: 'History'
+}
+
+let book3 = {
+    title: 'Minecraft: Guide to Survival',
+    pageCount: 96,
+    genre: 'Guide'
+}
+console.log(book1, book2, book3);
