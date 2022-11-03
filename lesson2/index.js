1c1eb1a0e69d45e497871c6ccbeb7dd7b74fd660
chiseDelta <ghostymyname@gmail.com>
2022-11-03 16:46:33 +0200

    створив 3 об'єкта з масивами. (щоб ще раз я шукав книги і їх авторів. P.S. Я знаю що це було не обовязково). 3 завдання готове

--- lesson2/index.js
+++ lesson2/index.js
@@ -23,3 +23,37 @@ let book3 = {
     genre: 'Guide'
 }
 console.log(book1, book2, book3);
+
+// Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+let newBook1 = {
+    title: 'Гарри Поттер и философский камень',
+    pageCount: 319,
+    genre: 'Фентези',
+    authors: [{
+        name: 'Джоан Роулінг',
+        age: 57
+    }]
+}
+
+let newBook2 = {
+    title: 'Lorem ipsum',
+    pageCount: 123,
+    genre: 'Наукова фантастика',
+    authors: [{
+        name: 'Дядя Саня',
+        age: 77
+    }]
+}
+
+let newBook3 = {
+    title: 'Идеальный программист',
+    pageCount: 224,
+    genre: 'Гайд',
+    authors: [{
+        name: 'Роберт Мартин',
+        age: 70
+    }]
+}
+
+console.log(newBook1, newBook2, newBook3);
