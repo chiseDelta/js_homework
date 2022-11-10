caa341847a8f640465865ffd94ee927727f5b29c
chiseDelta <ghostymyname@gmail.com>
2022-11-10 14:23:05 +0200

    переробив завдання 2

--- lesson3/main.js
+++ lesson3/main.js
@@ -15,14 +15,14 @@
 (в першу, другу, третю или четверту частину години).
  */
 
-// let time = +prompt('Write a number 0-59');
-// if (time <= 15) {
+// let time = +prompt('Write a number 0-59')
+// if (time >= 0 && time <= 15) {
 //     console.log('Перша четверть години')
-// } else if (time <= 30) {
+// } else if (time >= 16 && time <= 30) {
 //     console.log('Друга четверть години')
-// } else if (time <= 45) {
+// } else if (time >= 31 && time <= 45) {
 //     console.log('Третя четверть години')
-// } else if (time <= 59) {
+// } else if (time >= 46 && time <= 59) {
 //     console.log('Четверта четверть години')
 // } else {
 //     console.log('Шо?')
