c2748ec9910c4b7eaf1846a75ffdd8d279eafb96
chiseDelta <ghostymyname@gmail.com>
2022-11-23 14:07:36 +0200

    Finished task4

--- lesson6/main.js
+++ lesson6/main.js
@@ -27,13 +27,13 @@
 
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
-let str = ' dirty string   ';
-console.log(str.substring(1, 13));
+// let str = ' dirty string   ';
+// console.log(str.substring(1, 13));
 
 // let clearString = str.replaceAll(' ', '')
 // console.log(clearString);
 
-
+// !!Перший варіант забирає зайві пробіли з переду і ззаду!!
 
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
