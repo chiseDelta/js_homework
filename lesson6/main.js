000f38ecca869cbab758b938026f7cb0011ff484
chiseDelta <ghostymyname@gmail.com>
2022-11-23 13:53:00 +0200

    Finished task2

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,16 +1,22 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
 
-let arr = ['hello world', 'lorem ipsum', 'javascript is cool'];
-function findLength(array) {
-    for (const item of array) {
-        console.log(item.length)
-    }
-}
-findLength(arr)
+// let arr = ['hello world', 'lorem ipsum', 'javascript is cool'];
+// function findLength(array) {
+//     for (const item of array) {
+//         console.log(item.length)
+//     }
+// }
+// findLength(arr)
 
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let arr = ['hello world', 'lorem ipsum', 'javascript is cool'];
+for (const item of arr) {
+    console.log(item.toUpperCase())
+}
+
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
