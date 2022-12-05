7abb64d94c553f89457e3623aa7ae45b5ecdeeb6
chiseDelta <ghostymyname@gmail.com>
2022-12-05 17:16:47 +0200

    Finished task8

--- lesson6/main.js
+++ lesson6/main.js
@@ -59,32 +59,37 @@
 // sortNums(nums,'descending') // [21,11,3]
 //
 
-let nums = [11, 21, 3];
-
-function sortNums(arr, direction) {
-    if (direction === 'ascending') {
-        arr.sort((a, b) => a - b)
-    } else if (direction === 'descending') {
-        arr.sort((a, b) => b - a)
-    } else {
-        console.log('Wrong direction!')
-    }
-    return arr
-}
-console.log(sortNums(nums, 'ascending'));
+// let nums = [11, 21, 3];
+//
+// function sortNums(arr, direction) {
+//     if (direction === 'ascending') {
+//         arr.sort((a, b) => a - b)
+//     } else if (direction === 'descending') {
+//         arr.sort((a, b) => b - a)
+//     } else {
+//         console.log('Wrong direction!')
+//     }
+//     return arr
+// }
+// console.log(sortNums(nums, 'ascending'));
 
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // -- відсортувати його за спаданням за monthDuration
+
+coursesAndDurationArray.sort((a, b) =>
+    b.monthDuration - a.monthDuration)
+console.log(coursesAndDurationArray);
+
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//
+
 // описати колоду карт
 // - знайти піковий туз
 // - всі шістки
